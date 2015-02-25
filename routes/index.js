ec53559f1e6ac84380981b79d035981bf345ef5e
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2015-02-25 01:39:53 +0100

    [DEV] Added twitter route

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ router.get('/places', function(req, res, next) {
 	};
 
 	var requestUrl = createCompleteUrl(url, requestParams);
-	console.log('RequestUrl: ' + requestUrl);
+	console.log('PLACES/ requestUrl: ' + requestUrl)
 
 	request.get({url:requestUrl, oauth:oauth, json:true},function (error, response, body) {
 		if (!error && response.statusCode == 200) {
@@ -61,51 +61,23 @@ router.get('/places', function(req, res, next) {
 	});
 });
 
-/* GET foursquare request. */
-router.get('/places/foursquare', function(req, res, next) {
-	var url = 'https://api.foursquare.com/v2/venues/explore';
-
-	var requestParams = {
-		client_id: 'O0YY1XKVUHFMJY1B1Q04NHXMBAYLRS4IJRVLDWKYIKXER4AH',
-		client_secret: 'I3YZ5Y3UK20ZIDD5GMCBDR0ZMFJH0KWB5NRS1N03TMWAYAJW&v=20130815',
-		ll: req.query.loc, // example: '49.0,6.10'
-		radius: req.query.radius, //example '1000'
-		section: req.query.section,  //example 'food'
-		limit: 20
-	};
-
-	var requestUrl = createCompleteUrl(url,requestParams);
-	console.log('RequestUrl: ' + requestUrl);
-
-	request(requestUrl,function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			res.send(body);
-		}
-		if (error) {
-			console.log('Error: ' + error);
-			console.log('Status Code: ' + response.statusCode);
-			res.send(response.statusCode);
-		}
-	});
-});
-
 /* GET instagram place id */
-router.get('/places/instagram', function(req, res, next) {
+router.get('/media/instagram', function(req, res, next) {
 	var oauth = {
 		client_id: 'c40df6cf23aa448c9c2da9007284f8e6',
 		client_secret: '8f83ed86028a498185a05bb4277fe601'
 	}
-	
+
 	var url = 'https://api.instagram.com/v1/locations/search'
-	
+
 	var requestParams = {
 		lat: req.query.loc.split(',')[0],
 		lng: req.query.loc.split(',')[1]
 	}
-	
+
 	var requestUrl = createCompleteUrl(url, requestParams)
-	console.log('PLACES/INSTAGRAM requestUrl: ' + requestUrl)
-	
+	console.log('MEDIA/INSTAGRAM requestUrl: ' + requestUrl)
+
 	request.get({url:requestUrl, oauth:oauth, json:true}, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			res.send(body)
@@ -119,5 +91,63 @@ router.get('/places/instagram', function(req, res, next) {
 	})
 });
 
+/* GET twitter data */
+router.get('/media/twitter', function(req, res, next) {
+
+  var oauth = {
+    consumer_key: '7IYF9oKnPLDEta86RqtyehHVG',
+    consumer_secret: 'a7WnvHk0fOlRSEuvGVgcIvO9gsiYgRNZWb0wrQgkL9RQAUaKpz',
+    token: '3059932155-cle3iD7vkXjnd7bHHTPBMQrgVDv7YoFIxR2xx3t',
+    token_secret: 'TkdySo3XL17tIi1jBcYwpOhsLPMgiyBE5BAjXUtu3xfQd'
+  };
+
+  var url = 'https://api.twitter.com/1.1/search/tweets.json';
+  var requestParams = {
+    q: req.query.q,
+		geocode: req.query.loc+ ',1km'
+  };
+
+  var requestUrl = createCompleteUrl(url, requestParams);
+	console.log('MEDIA/TWITTER requestUrl: ' + requestUrl)
+
+  request.get({url:requestUrl, oauth:oauth, json:true},function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+    if (error) {
+      console.log('Error: ' + error);
+      console.log('Status Code: ' + response.statusCode);
+      res.send(response.statusCode);
+    }
+  });
+});
+/* GET foursquare request. */
+router.get('/places/foursquare', function(req, res, next) {
+  var url = 'https://api.foursquare.com/v2/venues/explore';
+
+  var requestParams = {
+    client_id: 'O0YY1XKVUHFMJY1B1Q04NHXMBAYLRS4IJRVLDWKYIKXER4AH',
+    client_secret: 'I3YZ5Y3UK20ZIDD5GMCBDR0ZMFJH0KWB5NRS1N03TMWAYAJW&v=20130815',
+    ll: req.query.loc, // example: '49.0,6.10'
+    radius: req.query.radius, //example '1000'
+    section: req.query.section,  //example 'food'
+    limit: 20
+  };
+
+  var requestUrl = createCompleteUrl(url,requestParams);
+  console.log('RequestUrl: ' + requestUrl);
+
+  request(requestUrl,function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+    if (error) {
+      console.log('Error: ' + error);
+      console.log('Status Code: ' + response.statusCode);
+      res.send(response.statusCode);
+    }
+  });
+
+});
 
 module.exports = router;
